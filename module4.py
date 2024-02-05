d28b1ddfeec65aea03b4d09c984574d16c85ddf0
YuZhan69 <60676754+YuZhan69@users.noreply.github.com>
2024-02-04 21:13:47 -0800

    ML Assignment 4.2

---
+++ module4.py
@@ -0,0 +1,19 @@
+print("Please input an positive number N:")
+N = int(input())
+print(f"Your input N is: {N}")
+
+print("Please input N positive numbers:")
+inputs = []
+for _ in range(N):
+    n = int(input())
+    inputs.append(n)
+print(f"Your inputs are {inputs}.")
+
+print("Please input an integer X:")
+X = int(input())
+
+print("THe index of X is:")
+if X not in inputs:
+    print("-1")
+else:
+    print(inputs.index(X) + 1)
